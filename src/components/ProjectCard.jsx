359efcbc6c0baef9d33548b569161a141240a947
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-01 01:10:19 +0500

    bg added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -36,7 +36,9 @@ const ProjectCard = () => {
                             featuredProjects && featuredProjects.map((project) => (
                                 <div className="p-4 md:w-1/3" key={project._id}>
                                     <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                        <img className="lg:h-80 md:h-36 w-full object-cover object-center" src={project.image.url} alt="project" />
+                                        <a target='blank' href={project.link}>
+                                            <img className="lg:h-80 md:h-36 w-full object-cover object-center" src={project.image.url} alt="project" />
+                                        </a>
                                         <div className="p-6">
                                             <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{project.category}</h2>
                                             <h1 className={`${darkMode && 'text-white'} title-font text-lg font-medium text-gray-900 mb-3`}>{project.title}</h1>
