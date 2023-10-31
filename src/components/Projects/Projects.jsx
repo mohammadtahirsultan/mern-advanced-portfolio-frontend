db4295995afee1fc09816a2eb2f8992e2a49c413
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-01 01:03:55 +0500

    bg added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -70,9 +70,12 @@ const ProjectCard = () => {
           {
             projects && projects.map((project) => (
               <div className="sm:w-1/2 mb-10 px-2" key={project._id}>
-                <Link to={project?.link} className="rounded-lg h-[53vh] overflow-hidden">
-                  <img alt="content" className="object-cover object-center h-full w-full" src={project.image.url} />
-                </Link>
+                <div className="rounded-lg h-[53vh] overflow-hidden">
+                  <Link to={project?.link} >
+                    <img alt="content" className="object-cover object-center h-full w-full" src={project.image.url} />
+                  </Link>
+
+                </div>
                 <h2 className={`${darkMode && 'text-white'} title-font text-2xl font-medium text-gray-900 mt-6 mb-3`}>{project.title}</h2>
                 <p className="leading-relaxed text-base px-8">{project.description}</p>
                 <button className={`${darkMode && 'bg-gray-600 hover:bg-gray-500'} flex mx-auto mt-6 text-white bg-gray-900 border-0 py-2 focus:outline-none hover:bg-gray-800 rounded px-8`}>
