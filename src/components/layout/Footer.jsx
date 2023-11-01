12a5b10dc8dd6cbcc242712471625157dc072804
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-01 12:27:07 +0500

    experience modified

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
                     <img src='/logo.webp' alt="Mohammad-Tahir" height={50} width={50} className='rounded-full' />
                     <span className={`${darkMode && 'text-white'} ml-3 text-xl`}>Ghareeb Star</span>
                 </a>
-                <p className={`${darkMode && 'text-white'} text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4`}>© {new Date().getFullYear()} Ghareeb Star —
+                <p className={`${darkMode && 'text-white'} text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4`}>© {new Date().getFullYear()} Ghareeb Star Programmer —
                     <a href="mailto:tahirsultanofficial@gmail.com" className={`${darkMode && 'text-white'}  text-gray-600 ml-1`} rel="noopener noreferrer" target="_blank">@tahirsultanofficial</a>
                 </p>
 
