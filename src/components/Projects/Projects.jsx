8e934bd84b223269137898a6a2962e0e5d611391
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-07 11:01:51 +0500

    about us - about me

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -70,16 +70,15 @@ const ProjectCard = () => {
           {
             projects && projects.map((project) => (
               <div className="sm:w-1/2 mb-10 px-2" key={project._id}>
-                <div className="rounded-lg h-[53vh] overflow-hidden">
+                <div className="rounded-lg w-full overflow-hidden">
                   <a target='blank' href={project?.link} >
-                    <img alt="content" className="object-cover object-center h-full w-full" src={project.image.url} />
+                    <img alt="content" className="object-cover object-center h-full w-full" src={project?.image.url} />
                   </a>
-
                 </div>
-                <h2 className={`${darkMode && 'text-white'} title-font text-2xl font-medium text-gray-900 mt-6 mb-3`}>{project.title}</h2>
-                <p className="leading-relaxed text-base px-8">{project.description}</p>
+                <h2 className={`${darkMode && 'text-white'} title-font text-2xl font-medium text-gray-900 mt-6 mb-3`}>{project?.title}</h2>
+                <p className="leading-relaxed text-base px-8">{project?.description}</p>
                 <button className={`${darkMode && 'bg-gray-600 hover:bg-gray-500'} flex mx-auto mt-6 text-white bg-gray-900 border-0 py-2 focus:outline-none hover:bg-gray-800 rounded px-8`}>
-                  <a href={project.link}>See the Demo </a>
+                  <a href={project?.link}>See the Demo </a>
                 </button>
               </div>
             ))
