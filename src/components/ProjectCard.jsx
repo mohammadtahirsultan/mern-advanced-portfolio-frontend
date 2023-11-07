a968ace95605ba23b293555f78419d48253fe2c2
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-07 11:21:24 +0500

    about us - about me

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -36,9 +36,11 @@ const ProjectCard = () => {
                             featuredProjects && featuredProjects.map((project) => (
                                 <div className="p-4 md:w-1/3" key={project._id}>
                                     <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                        <a target='blank' href={project.link}>
-                                            <img className="h-full w-full object-cover" src={project.image.url} alt="project" />
-                                        </a>
+                                        <div className="rounded-lg w-full h-full">
+                                            <a target='blank' href={project?.link} >
+                                                <img alt="content" className="w-full h-full object-cover" src={project?.image.url} />
+                                            </a>
+                                        </div>
                                         <div className="p-6">
                                             <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{project.category}</h2>
                                             <h1 className={`${darkMode && 'text-white'} title-font text-lg font-medium text-gray-900 mb-3`}>{project.title}</h1>
