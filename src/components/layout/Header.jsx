424324f0508de57831655c5ff4a0f81920107cd8
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-13 01:17:39 +0500

    Testimonials Schema Fixed

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -33,9 +33,9 @@ const Header = () => {
   const logoutHandle = async () => {
     await dispatch(logoutUser())
     navigate("/")
-    // setTimeout(() => {
-    //   refreshPage()
-    // }, 2000);
+    setTimeout(() => {
+      refreshPage()
+    }, 2000);
   }
 
   useEffect(() => {
