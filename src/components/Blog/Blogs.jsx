2dc43e45338e02bd65b703ca3394bc3d890d46fc
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-15 22:15:37 +0500

    Blogs to be Added

--- src/components/Blog/Blogs.jsx
+++ src/components/Blog/Blogs.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProjects } from '../../redux/actions/project';
+import Loader from '../Loader';
 
 const Blogs = () => {
   const getCarouselData = () => {
+
+
     return {
       currentIndex: 0,
       images: [
@@ -29,9 +34,22 @@ const Blogs = () => {
 
   let { currentIndex, images, increment } = getCarouselData();
 
+  const dispatch = useDispatch()
+  const { loading, error, featuredProjects } = useSelector(state => state.project)
+  const { darkMode } = useSelector((state) => state.theme);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error)
+      dispatch({ type: "clearError" })
+    }
+
+    dispatch(getAllProjects())
+  }, [error])
+
   return (
     <div className='bg-white'>
- 
+
 
       {/* Text Header */}
       <header className="w-full container mx-auto">
@@ -70,107 +88,54 @@ const Blogs = () => {
 
       <div className="container mx-auto flex flex-wrap py-6 ">
         {/* Posts Section */}
-        <section className="w-full md:w-2/3 flex flex-col items-center px-3 ">
-          <article className="flex flex-col shadow my-4">
-            {/* Article Image */}
-            <a href="#" className="hover:opacity-75">
-              <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="Article" />
-            </a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-              <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-              <p href="#" className="text-sm pb-3">
-                By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
-              </p>
-              <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-              <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            {/* Article Image */}
-            <a href="#" className="hover:opacity-75">
-              <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="Article" />
-            </a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-              <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-              <p href="#" className="text-sm pb-3">
-                By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
-              </p>
-              <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-              <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            {/* Article Image */}
-            <a href="#" className="hover:opacity-75">
-              <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="Article" />
-            </a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-              <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-              <p href="#" className="text-sm pb-3">
-                By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
-              </p>
-              <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-              <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            {/* Article Image */}
-            <a href="#" className="hover:opacity-75">
-              <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="Article" />
-            </a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-              <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-              <p href="#" className="text-sm pb-3">
-                By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
-              </p>
-              <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-              <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            {/* Article Image */}
-            <a href="#" className="hover:opacity-75">
-              <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="Article" />
-            </a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-              <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-              <p href="#" className="text-sm pb-3">
-                By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
-              </p>
-              <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-              <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            {/* Article Image */}
-            <a href="#" className="hover:opacity-75">
-              <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="Article" />
-            </a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-              <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-              <p href="#" className="text-sm pb-3">
-                By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
-              </p>
-              <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-              <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-            </div>
-          </article>
 
-          {/* More articles... */}
 
-          {/* Pagination */}
-          <div className="flex items-center py-8">
-            <a href="#" className="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center">1</a>
-            <a href="#" className="h-10 w-10 font-semibold text-gray-800 hover:bg-blue-600 hover:text-white text-sm flex items-center justify-center">2</a>
-            <a href="#" className="h-10 w-10 font-semibold text-gray-800 hover:text-gray-900 text-sm flex items-center justify-center ml-3">Next <i className="fas fa-arrow-right ml-2"></i></a>
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 md:py-24 mx-auto">
+
+
+            <div className="flex flex-wrap -m-4">
+
+              {
+                loading ? <div className='flex w-full justify-center'><Loader /> </div> :
+
+                  featuredProjects && featuredProjects.map((project) => (
+                    <div className="p-4 md:w-1/3" key={project._id}>
+                      <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <div className="rounded-lg w-full h-1/2">
+                          <a target='blank' href={project?.link} >
+                            <img alt="content" className="w-full h-full object-cover" src={project?.image.url} />
+                          </a>
+                        </div>
+                        <div className="p-6">
+                          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{project.category}</h2>
+                          <h1 className={`${darkMode && 'text-white'} title-font text-lg font-medium text-gray-900 mb-3`}>{project.title}</h1>
+                          <p className={`${darkMode && 'text-white'} leading-relaxed mb-3`}>{project.description}</p>
+                          <div className="flex items-center flex-wrap ">
+
+                            <button className={`${darkMode && 'bg-gray-600 hover:bg-gray-800'} flex mx-auto mt-6 text-white bg-gray-900 border-0 py-2 focus:outline-none hover:bg-gray-800 rounded px-8`}>
+                              <a target='blank' href={project.link}>See the Demo </a>
+
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                  ))
+              }
+
+
+
+
+
+            </div>
           </div>
         </section>
+
+
+
+
 
         {/* Sidebar Section */}
         <aside className="w-full md:w-1/3 flex flex-col items-center px-3">
@@ -202,7 +167,7 @@ const Blogs = () => {
         <div className="relative w-full flex items-center invisible md:visible md:pb-12">
           <button
             className="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
-            // onClick={decrement}
+          // onClick={decrement}
           >
             &#8592;
           </button>
