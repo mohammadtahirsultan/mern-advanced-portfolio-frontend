bc8ba874882a4584562c1992b0689b72387dbe2b
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-15 22:20:59 +0500

    Blogs to be Added

--- src/components/Blog/Blogs.jsx
+++ src/components/Blog/Blogs.jsx
@@ -155,15 +155,8 @@ const Blogs = () => {
             &#8594;
           </button>
         </div>
-        <div className="w-full container mx-auto flex flex-col items-center">
-          <div className="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
-            <a href="#" className="uppercase px-3">About Us</a>
-            <a href="#" className="uppercase px-3">Privacy Policy</a>
-            <a href="#" className="uppercase px-3">Terms & Conditions</a>
-            <a href="#" className="uppercase px-3">Contact Us</a>
-          </div>
-          <div className="uppercase pb-6">&copy; myblog.com</div>
-        </div>
+
+      
       </footer>
     </div>
   );
