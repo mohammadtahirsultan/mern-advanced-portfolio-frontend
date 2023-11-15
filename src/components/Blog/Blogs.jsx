dbb492fdacf310afe06194f84dc1d7a96e0c805d
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-15 22:19:33 +0500

    Blogs to be Added

--- src/components/Blog/Blogs.jsx
+++ src/components/Blog/Blogs.jsx
@@ -100,7 +100,7 @@ const Blogs = () => {
                 loading ? <div className='flex w-full justify-center'><Loader /> </div> :
 
                   featuredProjects && featuredProjects.map((project) => (
-                    <div className="p-4 md:w-1/2" key={project._id}>
+                    <div className="p-4 md:w-1/3" key={project._id}>
                       <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                         <div className="rounded-lg w-full h-1/2">
                           <a target='blank' href={project?.link} >
@@ -133,33 +133,7 @@ const Blogs = () => {
           </div>
         </section>
 
-
-
-
-
-        {/* Sidebar Section */}
-        <aside className="w-full md:w-1/3 flex flex-col items-center px-3">
-          <div className="w-full bg-white shadow flex flex-col my-4 p-6">
-            <p className="text-xl font-semibold pb-5">About Us</p>
-            <p className="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
-            <a href="#" className="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
-              Get to know us
-            </a>
-          </div>
-
-          {/* Instagram Section */}
-          <div className="w-full bg-white shadow flex flex-col my-4 p-6">
-            <p className="text-xl font-semibold pb-5">Instagram</p>
-            <div className="grid grid-cols-3 gap-3">
-              <img className="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=1" alt="Instagram Post" />
-              <img className="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=2" alt="Instagram Post" />
-              {/* More Instagram images... */}
-            </div>
-            <a href="#" className="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
-              <i className="fab fa-instagram mr-2"></i> Follow @dgrzyb
-            </a>
-          </div>
-        </aside>
+   
       </div>
 
       {/* Footer */}
