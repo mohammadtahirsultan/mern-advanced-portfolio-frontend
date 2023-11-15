c594e923dd4aa8f46c68d5046c0e8eac2156a62a
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-15 23:17:59 +0500

    Blogs to be Added

--- src/components/Blog/Post.jsx
+++ src/components/Blog/Post.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProjects } from '../../redux/actions/project';
+import toast from 'react-hot-toast';
 
 function Blog() {
     // const getCarouselData = () => {
@@ -26,41 +30,54 @@ function Blog() {
 
     let open = false; // Assuming this is declared somewhere
 
+    const dispatch = useDispatch()
+    const { loading, error, featuredProjects } = useSelector(state => state.project)
+    const { darkMode } = useSelector((state) => state.theme);
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error)
+            dispatch({ type: "clearError" })
+        }
+
+        //   dispatch(getAllProjects())
+    }, [error])
+
     return (
-        <div className="bg-white font-family-karla">
+        <div className="">
 
 
             {/* ... Text Header ... */}
             <header className="w-full container mx-auto">
                 <div className="flex flex-col items-center py-12">
-                <a className="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
-                        Ghareebstar Blog
-                    </a>
-                    <p className="text-lg text-gray-600">
+                    <h2 className={`${darkMode && 'text-white'} font-bold text-gray-800 uppercase text-5xl`}>
+                        Ghareebstar Blogs
+                    </h2>
+                    <p className={`${darkMode && 'text-white'} text-lg`}>
                         Lorem Ipsum Dolor Sit Amet
                     </p>
                 </div>
             </header>
 
             {/* ... Topic Nav ... */}
-            <nav className="w-full py-4 border-t border-b bg-gray-100" >
+            <nav className={"w-full py-4 border-t border-b"} >
                 <div className="block sm:hidden">
                     <a
                         href="#"
-                        className="md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
+                        className="md:flex text-base font-bold uppercase text-center flex justify-center items-center"
                         onClick={() => open = !open}
                     >
                         Topics <i className={`fas ml-2 ${open ? 'fa-chevron-down' : 'fa-chevron-up'}`}></i>
                     </a>
                 </div>
-                <div className={`w-full ${open ? 'flex-grow sm:flex sm:items-center sm:w-auto' : 'hidden'}`}>
+                <div className={`w-full ${open ? 'flex-grow sm:flex sm:items-center sm:w-auto' : 'block'}`}>
                     <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-                        <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
-                        <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
-                        <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
-                        <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>
-                        <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>
-                        <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>
+                        <a href="#" className={`${darkMode && 'hover:bg-gray-700'} hover:bg-gray-200 rounded py-2 px-6 mx-2`}>Technology</a>
+                        <a href="#" className={`${darkMode && 'hover:bg-gray-700'} hover:bg-gray-200 rounded py-2 px-6 mx-2`}>Automotive</a>
+                        <a href="#" className={`${darkMode && 'hover:bg-gray-700'} hover:bg-gray-200 rounded py-2 px-6 mx-2`}>Finance</a>
+                        <a href="#" className={`${darkMode && 'hover:bg-gray-700'} hover:bg-gray-200 rounded py-2 px-6 mx-2`}>Politics</a>
+                        <a href="#" className={`${darkMode && 'hover:bg-gray-700'} hover:bg-gray-200 rounded py-2 px-6 mx-2`}>Culture</a>
+                        <a href="#" className={`${darkMode && 'hover:bg-gray-700'} hover:bg-gray-200 rounded py-2 px-6 mx-2`}>Sports</a>
                     </div>
                 </div>
             </nav>
@@ -77,8 +94,8 @@ function Blog() {
                         <a href="#" className="hover:opacity-75">
                             <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="Article" />
                         </a>
-                        <div className="bg-white flex flex-col justify-start p-6">
-                            <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
+                        <div className=" flex flex-col justify-start p-6">
+                            <a href="#" className="text-sm font-bold uppercase pb-4">Technology</a>
                             <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
                             <p href="#" className="text-sm pb-8">
                                 By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
@@ -91,18 +108,18 @@ function Blog() {
 
                     {/* ... Previous/Next Links ... */}
                     <div className="w-full flex pt-6">
-                        <a href="#" className="w-1/2 bg-white shadow hover:shadow-md text-left p-6">
-                            <p className="text-lg text-blue-800 font-bold flex items-center"><i className="fas fa-arrow-left pr-1"></i> Previous</p>
+                        <a href="#" className="w-1/2 shadow hover:shadow-md text-left p-6">
+                            <p className="text-lg font-bold flex items-center"><i className="fas fa-arrow-left pr-1"></i> Previous</p>
                             <p className="pt-2">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</p>
                         </a>
-                        <a href="#" className="w-1/2 bg-white shadow hover:shadow-md text-right p-6">
-                            <p className="text-lg text-blue-800 font-bold flex items-center justify-end">Next <i className="fas fa-arrow-right pl-1"></i></p>
+                        <a href="#" className="w-1/2 shadow hover:shadow-md text-right p-6">
+                            <p className="text-lg font-bold flex items-center justify-end">Next <i className="fas fa-arrow-right pl-1"></i></p>
                             <p className="pt-2">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</p>
                         </a>
                     </div>
 
                     {/* ... Author Section ... */}
-                    <div className="w-full flex flex-col text-center md:text-left md:flex-row shadow bg-white mt-10 mb-10 p-6">
+                    <div className="w-full flex flex-col text-center md:text-left md:flex-row shadow mt-10 mb-10 p-6">
                         <div className="w-full md:w-1/5 flex justify-center md:justify-start pb-4">
                             <img src="https://source.unsplash.com/collection/1346951/150x150?sig=1" className="rounded-full shadow h-32 w-32" alt="Author" />
                         </div>
@@ -110,7 +127,7 @@ function Blog() {
                             <p className="font-semibold text-2xl">David</p>
                             <p className="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel neque non libero suscipit suscipit eu eu urna.</p>
                             {/* ... Social Icons ... */}
-                            <div className="flex items-center justify-center md:justify-start text-2xl no-underline text-blue-800 pt-4">
+                            <div className="flex items-center justify-center md:justify-start text-2xl no-underline pt-4">
                                 <a className="" href="#">
                                     <i className="fab fa-facebook"></i>
                                 </a>
@@ -131,21 +148,21 @@ function Blog() {
                 {/* ... Sidebar Section ... */}
                 <aside className="w-full md:w-1/3 flex flex-col items-center px-3">
                     {/* ... About Us Section ... */}
-                    <div className="w-full bg-white shadow flex flex-col my-4 p-6">
+                    <div className="w-full shadow flex flex-col my-4 p-6">
                         <p className="text-xl font-semibold pb-5">About Us</p>
                         <p className="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
-                        <a href="#" className="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+                        <a href="#" className="w-full text-white font-bold text-sm uppercase rounded flex items-center justify-center px-2 py-3 mt-4">
                             Get to know us
                         </a>
                     </div>
 
                     {/* ... Instagram Section ... */}
-                    <div className="w-full bg-white shadow flex flex-col my-4 p-6">
+                    <div className="w-full  shadow flex flex-col my-4 p-6">
                         <p className="text-xl font-semibold pb-5">Instagram</p>
                         <div className="grid grid-cols-3 gap-3">
                             {/* ... Instagram Images ... */}
                         </div>
-                        <a href="#" className="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
+                        <a href="#" className="w-full font-bold text-sm uppercase rounded flex items-center justify-center px-2 py-3 mt-6">
                             <i className="fab fa-instagram mr-2"></i> Follow @dgrzyb
                         </a>
                     </div>
@@ -153,33 +170,27 @@ function Blog() {
 
             </div>
 
-            <footer className="w-full border-t bg-white pb-12">
+
+            <footer className="w-full border-t pb-12">
                 <div className="relative w-full flex items-center invisible md:visible md:pb-12">
                     <button
-                        className="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
-                        // onClick={decrement}
+                        className="absolut text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
+                    // onClick={decrement}
                     >
                         &#8592;
                     </button>
-                    {/* {images?.slice(currentIndex, currentIndex + 6)?.map((image, index) => (
+                    {/* {images && images.slice(currentIndex, currentIndex + 6).map((image, index) => (
                         <img key={index} className="w-1/6 hover:opacity-75" src={image} alt={`Carousel Image ${index}`} />
                     ))} */}
                     <button
-                        className="absolute right-0 bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
+                        className="absolute right-0 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
                         // onClick={increment}
                     >
                         &#8594;
                     </button>
                 </div>
-                <div className="w-full container mx-auto flex flex-col items-center">
-                    <div className="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
-                        <a href="#" className="uppercase px-3">About Us</a>
-                        <a href="#" className="uppercase px-3">Privacy Policy</a>
-                        <a href="#" className="uppercase px-3">Terms & Conditions</a>
-                        <a href="#" className="uppercase px-3">Contact Us</a>
-                    </div>
-                    <div className="uppercase pb-6">&copy; myblog.com</div>
-                </div>
+
+
             </footer>
         </div>
     );
