bc196e29883039d107285c813f8917ede78e894d
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-16 12:53:36 +0500

    Blogs and Category Come into Being

--- src/admin/Sidebar.jsx
+++ src/admin/Sidebar.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { logoutUser } from '../redux/actions/user';
 import { toast } from 'react-hot-toast';
 import { toggleTheme } from '../redux/reducers/theme';
+import { Home } from '@mui/icons-material';
 
 const Sidebar = () => {
 
@@ -47,21 +48,7 @@ const Sidebar = () => {
                                     to="/dashboard"
                                     className="flex items-center p-2 space-x-3 rounded-md"
                                 >
-                                    {/* <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                                        />
-                                    </svg> */}
-
+                                   <Home className='h-6 w-6' />
                                     <img src="/home.png" alt="home" className='h-6 w-6' />
                                     <span>Home</span>
                                 </Link>
