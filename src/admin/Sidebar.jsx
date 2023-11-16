133648c85cc808c2fc031456f702ac1071f6507d
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-16 10:43:48 +0500

    Bg-Changed

--- src/admin/Sidebar.jsx
+++ src/admin/Sidebar.jsx
@@ -42,7 +42,7 @@ const Sidebar = () => {
 
 
         <>
-            <div className="flex flex-col h-screen p-3 bg-white shadow w-60">
+            <div className="flex flex-col h-screen p-3 shadow w-60">
                 <div className="space-y-3">
                     <div className="flex items-center">
                         <h2 className="text-xl font-bold">Dashboard</h2>
