c38751c0984538056c5653c46220c74cf5a130d8
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-19 05:21:46 +0500

    ook...

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -69,7 +69,7 @@ const ProjectCard = () => {
         loading ? <div className='flex w-screen justify-center' ><Loader /> </div> : <>
           {
             projects && projects.map((project) => (
-              <div className="sm:w-1/3 mb-10 px-2" key={project._id}>
+              <div className="sm:w-1/3 mb-10 px-4" key={project._id}>
                 <div className="rounded-lg w-full h-2/3">
                   <a target='blank' href={project?.link} >
                     <img alt="content" className="w-full h-full object-cover" src={project?.image.url} />
