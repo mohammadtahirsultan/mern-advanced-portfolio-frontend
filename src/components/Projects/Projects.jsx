c327d94b0121f3754cd7050e76c78a090b2e90f5
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-19 05:46:26 +0500

    projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -71,7 +71,7 @@ const ProjectCard = () => {
             projects && projects.map((project) => (
               <div className="p-4 md:w-1/3 rounded-lg" key={project._id}>
                 <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                  <div className="rounded-lg w-full h-1/2">
+                  <div className="rounded-lg w-full h-1/3">
                     <a target='blank' href={project?.link} >
                       <img alt="content" className="w-full h-full object-cover" src={project?.image.url} />
                     </a>
