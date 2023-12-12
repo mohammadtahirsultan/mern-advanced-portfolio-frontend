53de0f0b00712daadf0d0019c93f5241b09e4bc8
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-12-12 16:27:09 +0500

    Edit Project Done...

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ import EditBlog from './admin/EditBlog'
 import AddCategory from './admin/AddCategory.jsx'
 import Categories from './admin/Categories.jsx'
 import EditCategory from './admin/EditCategory.jsx'
+import { useAuth0 } from '@auth0/auth0-react'
 
 
 
@@ -60,8 +61,9 @@ const HeaderWithRoutes = () => {
 
   const { darkMode } = useSelector((state) => state.theme);
 
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-  const { isAuthenticated, user } = useSelector(state => state.user)
+  // const { isAuthenticated, user } = useSelector(state => state.user)
 
   const location = useLocation();
   const showHeader = !location.pathname.startsWith('/dashboard');
