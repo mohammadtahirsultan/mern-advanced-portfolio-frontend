2e2c0af98758f5fee67a031c099d31596bdf85d4
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-12-29 06:28:18 +0500

    frontend link added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,10 +11,10 @@ const Project = () => {
   const { darkMode } = useSelector((state) => state.theme);
 
   return (
-    <section className="text-gray-600 body-font min-h-screen">
+    <section className="body-font min-h-screen">
       <div className="container px-5 py-16 mx-auto">
 
-        <h2 className={`${darkMode && 'text-gray-400'} mb-6 text-3xl md:text-5xl text-center font-bold`}>
+        <h2 className={`mb-6 text-3xl md:text-5xl text-center font-bold`}>
           <Typewriter
             options={{
               strings: "Projects",
@@ -24,9 +24,9 @@ const Project = () => {
           />
 
         </h2>
-        <hr className={`${darkMode && 'bg-gray-400'} bg-black w-24 py-0.5 mx-auto mb-16`} />
+        <hr className={`bg-black w-24 py-0.5 mx-auto mb-16`} />
 
-        <div className={`${darkMode && 'text-gray-200'} flex flex-wrap -mx-4 -mb-10 text-center`}>
+        <div className={`flex flex-wrap -mx-4 -mb-10 text-center`}>
 
 
           <ProjectCard />
@@ -53,7 +53,6 @@ const ProjectCard = () => {
   const { darkMode } = useSelector((state) => state.theme);
 
 
-
   useEffect(() => {
     if (error) {
       toast.error(error)
@@ -70,19 +69,19 @@ const ProjectCard = () => {
           {
             projects && projects.map((project) => (
               <div className="p-4 md:w-1/3 rounded-lg" key={project._id}>
-                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <div className="h-full border-2 border-opacity-60 rounded-lg overflow-hidden">
                   <div className="rounded-lg w-full h-1/3 md:h-1/2">
                     <a target='blank' href={project?.link} >
                       <img alt="content" className="w-full h-full object-cover" src={project?.image.url} />
                     </a>
                   </div>
                   <div className="p-6">
-                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{project.category}</h2>
-                    <h1 className={`${darkMode && 'text-white'} title-font text-lg font-medium text-gray-900 mb-3`}>{project.title}</h1>
-                    <p className={`${darkMode && 'text-white'} leading-relaxed mb-3`}>{project.description}</p>
+                    <h2 className="tracking-widest text-xs title-font font-mediummb-1">{project.category}</h2>
+                    <h1 className={`title-font text-lg font-medium mb-3`}>{project.title}</h1>
+                    <p className={`leading-relaxed mb-3`}>{project.description}</p>
                     <div className="flex items-center flex-wrap ">
 
-                      <button className={`${darkMode && 'bg-gray-600 hover:bg-gray-800'} flex mx-auto mt-6 text-white bg-gray-900 border-0 py-2 focus:outline-none rounded px-8`}>
+                      <button className={` flex mx-auto mt-6 border-0 py-2 focus:outline-none rounded px-8`}>
                         <a target='blank' href={project.link}>See the Demo </a>
                       </button>
                     </div>
